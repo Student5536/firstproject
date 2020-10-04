f231f88a3d95816972994c7def528c34ab2fc2dc
Ömer Kurt <mer Kurt@192.168.2.110>
2020-10-04 12:52:24 +0200

    schleife

--- firstproject/src/firstproject/Test.java
+++ firstproject/src/firstproject/Test.java
@@ -5,5 +5,9 @@ public class Test {
 	public static void main(String[]args) {
 		
 		System.out.println("My first project: GitHub");
+		
+		for(int i =0; i < 10; i++) {
+			System.out.println("Das neue Semester hat angefangen");
+		}
 	}
 }
